5fde236dbf3b09f10ad355933c029fec753b68ce
Ahmad Busari <ahmadinjo@users.noreply.github.com>
2020-09-17 11:00:16 +0100

    Task 4 gitted

--- travello/dbtest.sql
+++ travello/dbtest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 11, 2020 at 04:21 PM
+-- Generation Time: Sep 14, 2020 at 01:51 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -30,10 +30,27 @@ SET time_zone = "+00:00";
 CREATE TABLE `destination` (
   `ID` int(2) NOT NULL,
   `Name` varchar(255) NOT NULL,
-  `Description` varchar(255) DEFAULT NULL,
+  `Description` varchar(255) NOT NULL,
   `City` varchar(255) NOT NULL,
-  `Budget` int(100) NOT NULL
+  `Budget` int(11) NOT NULL,
+  `Offer` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `destination`
+--
+
+INSERT INTO `destination` (`ID`, `Name`, `Description`, `City`, `Budget`, `Offer`, `image`) VALUES
+(1, 'Palm Resorts', 'Nulla pretium tincidunt felis nec', 'Bali', 679, 'Special', 'destination_1.jpg'),
+(2, 'Indonesia National Park', 'Nulla pretium tincidunt felis, nec', 'Indonesia', 579, 'Normal', 'destination_2.jpg'),
+(3, 'The State Bridge', 'Nulla pretium tincidunt felis, nec', 'San Francisco', 399, 'Special', 'destination_3.jpg'),
+(4, 'The Great Entrance Of Babel', 'Nulla pretium tincidunt felis, nec', 'Paris', 639, 'Normal', 'destination_4.jpg'),
+(5, 'Phi Phi Island Resort', 'Nulla pretium tincidunt felis, nec', 'Phi Phi Island', 929, 'Special', 'destination_5.jpg'),
+(6, 'Kings Platform Estate', 'Nulla pretium tincidunt felis, nec', 'Mykonos', 719, 'Normal', 'destination_6.jpg'),
+(7, 'Ship Island', 'Nulla pretium tincidunt felis, nec', 'Paris', 515, 'Normal', 'destination_7.jpg'),
+(8, 'Phi Phi Island Slim Bridge', 'Nulla pretium tincidunt felis, nec', 'Phi Phi Island', 879, 'Normal', 'destination_8.jpg'),
+(9, 'Mykonos Snake Lake', 'Nulla pretium tincidunt felis, nec', 'Mykonos', 679, 'Normal', 'destination_9.jpg');
 
 -- --------------------------------------------------------
 
@@ -58,10 +75,10 @@ INSERT INTO `test_customer_info` (`ID`, `FirstName`, `LastName`, `Username`, `Pa
 (1, 'Ahmad', 'Busari', 'ahmadinjo', '499172AFRica @', 'dadman4life@gmail.com'),
 (2, 'Maryam', 'Busari', 'Maryet', 'Simple password = 1234', 'maryet@gmail.com'),
 (3, 'Lagbaja', 'Tamedo', 'lagbaja', '1Q2W3E4R5T6Y7u8i9o0p', 'lagbajatamedo@yahoo.com'),
-(5, 'Taofeeq', 'Mashood', 'taofeeq124', 'Taofeeq124', 'taofeeq124@gmail.com'),
-(6, 'John', 'Doe', 'johndoe', 'Asdfg12345', 'johndoe@aol.com'),
-(7, 'Jane', 'Doe', 'janedoe', 'Asdfg12345', 'janedoe@hotmail.com'),
-(8, 'Faseeki', 'Temitope', 'faseeki1', '0Sssssss', 'faseekiswenky@gmail.com');
+(4, 'Taofeeq', 'Mashood', 'taofeeq124', 'Taofeeq124', 'taofeeq124@gmail.com'),
+(5, 'John', 'Doe', 'johndoe', 'Asdfg12345', 'johndoe@aol.com'),
+(6, 'Jane', 'Doe', 'janedoe', 'Asdfg12345', 'janedoe@hotmail.com'),
+(7, 'Faseeki', 'Temitope', 'faseeki1', '0Sssssss', 'faseekiswenky@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -89,13 +106,13 @@ ALTER TABLE `test_customer_info`
 -- AUTO_INCREMENT for table `destination`
 --
 ALTER TABLE `destination`
-  MODIFY `ID` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `test_customer_info`
 --
 ALTER TABLE `test_customer_info`
-  MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
